bfe6dad869ee952c73b3419e4b19af91b0e2bb98
mohammadabukhater <m.abukhater@yahoo.com>
2020-12-29 04:16:30 +0200

    about-me

---
+++ project-directory/js/app.js
@@ -0,0 +1,51 @@
+'use strict';
+var username =prompt('please tell me what is your name ?')
+alert(username +" we are glad to have you here :)");
+
+var myfavcolor =prompt('is my favourite color blue? (yes/no)');
+//console.log('yes');
+
+if (myfavcolor.toLowerCase() === 'yes'){
+  //  console.log('I am so glad that u got it :) ');
+  alert('I am so glad that u got it :) ');
+}   
+else {
+  //  console.log('try again pleaes :(');
+  alert('try again pleaes :(');
+}
+var myfavsport =prompt('is my favourite sport is football? (yes/no)');
+//console.log('no');
+if (myfavsport.toUpperCase() === 'YES') {
+   // console.log('try one more time');
+   alert('try one more time');
+} else {
+   // console.log('good job u got it');
+   alert('good job u got it');
+}
+var personalexp =prompt('do you think that i am calm person? (yes/no)');
+//console.log('no');
+if (personalexp.toLowerCase() === "yes") {
+  //  console.log('indeed your good person but you are wrong');
+  alert('indeed your good person but you are wrong');
+} else {
+  //  console.log('well that is me :p');
+  alert('well that is me :p');
+}
+var personalatit =prompt('do I look like person who loves reading? (yes/no)');
+//console.log('yes');
+if (personalatit.toUpperCase() ==='YES' ) {
+   // console.log('well who is not inlove with reading');  
+   alert('well who is not inlove with reading'); 
+} else {
+  //  console.log('no no no again please');
+  alert('no no no again please');
+}
+var healthypers =prompt('do you think that I am smoker or nonsmoker? (yes/no)');
+//console.log('yes');
+if (healthypers.toLowerCase() === 'yes') {
+  //  console.log('I know that it is harmful but yeah you are right');
+  alert('I know that it is harmful but yeah you are right');    
+} else {
+    //console.log('I wish it is no but try again');
+    alert('I wish it is no but try again');
+}
